8013a10074086b750682a3f7c333ef596d88fd13
unknown <roee1454@gmail.com>
2024-03-31 23:51:44 +0300

    A

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
 import "./styles/index.css";
-import axios from "axios";
-import { v4 } from "uuid";
-
-const API_KEY = "CG-HFgNR2dU6iXjsU3BzkBXWtsk";
-
-interface CryptoCurrency {
-    
-}
-
-async function fetchYahooStocksData() {
-  const { data } = await axios.get("");
-  return data;
-}
